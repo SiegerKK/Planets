044355c57fa2ea6ac74531d021b5ae66c539b8bf
Sieger <khloponin.kirill@gmail.com>
2016-06-11 08:52:47 +0200

    Added "Main menu"

---
+++ Assets/scripts/ScenesController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenesController : MonoBehaviour {
+	public void loadScene1(){
+		Application.LoadLevel ("test1");
+	}
+	public void loadScene2(){
+		Application.LoadLevel ("test2");
+	}
+	public void loadScene3(){
+		Application.LoadLevel ("test3");
+	}
+}
